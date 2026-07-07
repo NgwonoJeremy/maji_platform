d8870c01627f647d240e628c13d69dfb1356bb8e
NgwonoJeremy <jeremyngwono7174@gmail.com>
2026-07-07 18:42:52 +0300

    Changing App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,25 @@
-import { BrowserRouter , Routes, Route } from 'react-router-dom';
 import 'leaflet/dist/leaflet.css';
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Home from "./Pages/Home";
+import Roles from "./Pages/Roles";
 import CustomerDashboard from "./Pages/customer/CustomerDashboard";
 import AdminDashboard from './Pages/admin/Admin_dash';
 import AllOrders from './Pages/admin/AllOrders';
 import AllVendors from './Pages/admin/AllVendors';
-import Vendors from '.Pages/'
-import './App.css';
+import VendorRegister from './Pages/vendor/VendorRegister';
+import VendorDashboard from './Pages/vendor/VendorDashboard';
 
 function App() {
   return (
     <div className="app">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<AdminDashboard />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/roles" element={<Roles />} />
+          <Route path="/customer" element={<CustomerDashboard />} />
+          <Route path="/admin" element={<AdminDashboard />} />
           <Route path="/orders" element={<AllOrders />} />
           <Route path="/vendors" element={<AllVendors />} />
           <Route path="/vendor/register" element={<VendorRegister />} />
